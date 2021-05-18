f4eca528deb1f764881e71ebd2b097d7ec396ac3
Alexander Nichiporenko <alexander.nichiporenko@skillbox.ru>
2021-05-18 05:46:51 +0300

    Install fontawesome icons

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
