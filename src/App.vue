6a6d4ddbb33da849f3c6819fc22f08906e250fdc
Aleksandr Nichiporenko <aleksandr.nichiporenko@coinspaid.com>
2021-06-08 08:36:55 +0300

    Login

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/users">Users</router-link> |
+    <router-link to="/login">Login</router-link> |
     <router-link to="/register">Register</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
 </template>
+
 
 <style lang="scss">
 #app {
---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="register">
+    <h1>Login</h1>
+    <form class="login-form" @submit.prevent>
+      <div class="form-element">
+        Email:
+        <input type="text" placeholder="email" v-model="form.email" />
+      </div>
+      <div class="form-element">
+        Password:
+        <input type="text" placeholder="password" v-model="form.password" />
+      </div>
+      <div class="form-element">
+        <button class="btn" @click="submit">Login</button>
+      </div>
+      <Error :error="error"/>
+    </form>
+  </div>
+</template>
+
+<script>
+import Error from "@/components/UI/Error"
+
+export default {
+  name: "Register",
+  inheritAttrs: false,
+  components: {
+    Error,
+  },
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+      error: ""
+    };
+  },
+  methods: {
+    submit() {
+        this.clearError()
+        this.$http.post("login", this.form)
+          .then(() => this.$router.push({ name: "Home"})) 
+          .catch((err) => this.handleError(err.response)) 
+    },
+    handleError(err) {
+      this.error = err.data
+    },
+    clearError() {
+      this.error = ""
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.login-form {
+  margin: 0 auto;
+  padding: 8px 24px;
+  width: 400px;
+  border: 1px solid rgb(222, 222, 222);
+  border-radius: 8px;
+}
+.form-element {
+  margin-top: 8px;
+  display: flex;
+  justify-content: space-between;
+}
+input {
+  padding: 4px 8px;
+  border: 1px solid rgb(222, 222, 222);
+  border-radius: 4px;
+}
+.btn {
+  padding: 4px 8px;
+  background-color: #42b983;
+  color: #ffffff;
+  border: 1px solid rgb(222, 222, 222);
+  border-radius: 4px;
+}
+</style>
